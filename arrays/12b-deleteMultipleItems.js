3340af86961a0df37ac7a816eac6852e05cc0626
K. Papadopoulos <kostashope@hotmail.com>
2025-02-01 11:26:52 +0200

    updates

--- arrays/12b-deleteMultipleItems.js
+++ arrays/12b-deleteMultipleItems.js
@@ -2,13 +2,12 @@ const arr = [1, 2, 3, 2, 5, 2]
 
 const deleteBackwards = (arr, val) => {
   if (!arr) return
-  let i = arr.length - 1
+  let i = arr.length
 
-  while (i) {
+  while (i--) {
     if (arr[i] === val) {
       arr.splice(i, 1)
     }
-    i--
   }
 }
 
